0cac2a510514200b3fd23817055bc1776ec4b8db
igor-cavalcante <igorcavalcantealbuquerque@gmail.com>
2026-03-12 10:48:28 -0300

    feature: Mudando nome do site

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Gestão de Frotas',
+      title: 'VIGIA',
       locale: const Locale('pt', 'BR'),
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
